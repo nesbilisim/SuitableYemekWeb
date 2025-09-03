1794d309c4aa8ced596df93d6c51dc4ab130e586
anilduran <durananil@outlook.com>
2025-09-03 14:41:30 +0300

    Remove About and Contact pages, and update routing in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,6 @@ import './index.css'
 import App from './App.tsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { Home } from './pages/Home.tsx'
-import { About } from './pages/About.tsx'
-import { Contact } from './pages/Contact.tsx'
 
 
 const router = createBrowserRouter([
@@ -16,14 +14,6 @@ const router = createBrowserRouter([
       {
         path: '',
         element: <Home />
-      },
-      {
-        path: 'about',
-        element: <About />
-      },
-      {
-        path: 'contact',
-        element: <Contact />
       }
     ]
   }
